647ed06fc73b17eccd959748270c647df72f0754
Ha-YeJin <ziny7505k@gmail.com>
2025-04-30 15:02:33 +0900

    feat(utils): add Korean class and phoneme2index generator

---
+++ utils/phoneme_utils.py
@@ -0,0 +1,59 @@
+import json
+
+class Korean:
+    unicode_base_code, unicode_onset_offset, unicode_nucleus_offset = 44032, 588, 28
+    phoneme_onset_list = ['ㄱ', 'ㄲ', 'ㄴ', 'ㄷ', 'ㄸ', 'ㄹ', 'ㅁ', 'ㅂ', 'ㅃ', 'ㅅ', 'ㅆ', 'ㅇ', 'ㅈ', 'ㅉ', 'ㅊ', 'ㅋ', 'ㅌ', 'ㅍ', 'ㅎ']
+    phoneme_nucleus_list = ['ㅏ', 'ㅐ', 'ㅑ', 'ㅒ', 'ㅓ', 'ㅔ', 'ㅕ', 'ㅖ', 'ㅗ', 'ㅘ', 'ㅙ', 'ㅚ', 'ㅛ', 'ㅜ', 'ㅝ', 'ㅞ', 'ㅟ', 'ㅠ', 'ㅡ', 'ㅢ', 'ㅣ']
+    phoneme_coda_list = [' ', 'ㄱ', 'ㄲ', 'ㄳ', 'ㄴ', 'ㄵ', 'ㄶ', 'ㄷ', 'ㄹ', 'ㄺ', 'ㄻ', 'ㄼ', 'ㄽ', 'ㄾ', 'ㄿ', 'ㅀ', 'ㅁ', 'ㅂ', 'ㅄ', 'ㅅ', 'ㅆ', 'ㅇ', 'ㅈ', 'ㅊ', 'ㅋ', 'ㅌ', 'ㅍ', 'ㅎ']
+    phoneme_double_consonant_dict = {
+        'ㄲ': ['ㄱ', 'ㄱ'], 'ㄳ': ['ㄱ', 'ㅅ'], 'ㄵ': ['ㄴ', 'ㅈ'], 'ㄶ': ['ㄴ', 'ㅎ'],
+        'ㄸ': ['ㄷ', 'ㄷ'], 'ㄺ': ['ㄹ', 'ㄱ'], 'ㄻ': ['ㄹ', 'ㅁ'], 'ㄼ': ['ㄹ', 'ㅂ'],
+        'ㄽ': ['ㄹ', 'ㅅ'], 'ㄾ': ['ㄹ', 'ㅌ'], 'ㄿ': ['ㄹ', 'ㅍ'], 'ㅀ': ['ㄹ', 'ㅎ'],
+        'ㅃ': ['ㅂ', 'ㅂ'], 'ㅄ': ['ㅂ', 'ㅅ'], 'ㅆ': ['ㅅ', 'ㅅ'], 'ㅉ': ['ㅈ', 'ㅈ']
+    }
+
+    @staticmethod
+    def is_korean(char):
+        code = ord(char)
+        return 0xAC00 <= code <= 0xD7A3
+
+    @staticmethod
+    def decompose_syllable(char):
+        base = ord(char) - Korean.unicode_base_code
+        onset = base // Korean.unicode_onset_offset
+        nucleus = (base % Korean.unicode_onset_offset) // Korean.unicode_nucleus_offset
+        coda = base % Korean.unicode_nucleus_offset
+        o = Korean.phoneme_onset_list[onset]
+        n = Korean.phoneme_nucleus_list[nucleus]
+        c = Korean.phoneme_coda_list[coda] if coda > 0 else None
+        return o, n, c
+
+    @staticmethod
+    def text_to_phoneme_sequence(text, phoneme2index):
+        seq = []
+        for char in text:
+            if not Korean.is_korean(char): continue
+            o, n, c = Korean.decompose_syllable(char)
+            if o in Korean.phoneme_double_consonant_dict:
+                for p in Korean.phoneme_double_consonant_dict[o]:
+                    seq.append(phoneme2index.get(p, 0))
+            elif o:
+                seq.append(phoneme2index.get(o, 0))
+            if n: seq.append(phoneme2index.get(n, 0))
+            if c in Korean.phoneme_double_consonant_dict:
+                for p in Korean.phoneme_double_consonant_dict[c]:
+                    seq.append(phoneme2index.get(p, 0))
+            elif c:
+                seq.append(phoneme2index.get(c, 0))
+        return seq
+
+# Generate phoneme2index dictionary
+phoneme_set = set(Korean.phoneme_onset_list + Korean.phoneme_nucleus_list + Korean.phoneme_coda_list)
+phoneme_set.discard(' ')
+phoneme_list = sorted(phoneme_set)
+phoneme2index = {p: i + 1 for i, p in enumerate(phoneme_list)}
+phoneme2index['<blank>'] = 0
+
+# Save dictionary to JSON
+with open('phoneme2index.json', 'w', encoding='utf-8') as f:
+    json.dump(phoneme2index, f, ensure_ascii=False, indent=4)
