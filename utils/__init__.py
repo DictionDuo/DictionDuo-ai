d67d5b43de9d7dd68db7f975f086f4c72287c2b7
Ha-YeJin <ziny7505k@gmail.com>
2025-05-05 16:04:19 +0900

    chore(utils): expose convert_config_to_namespace in utils package

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,3 +1,4 @@
 from .phoneme_utils import Korean, phoneme2index
 from .seed import set_seed, get_data_loader
 from .logger import setup_logger
+from .config_loader import convert_config_to_namespace
