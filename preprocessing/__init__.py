392f95e01bb9714fee0791e0e109f15da512fc7c
Ha-YeJin <ziny7505k@gmail.com>
2025-05-26 16:23:41 +0900

    refactor: import get_max_lengths from build_dataset

--- preprocessing/__init__.py
+++ preprocessing/__init__.py
@@ -1,3 +1,3 @@
 from .split_dataset import build_and_split, split_metadata
 from .feature_extraction import extract_features
-from .build_dataset import build_metadata_list
+from .build_dataset import build_metadata_list, get_max_lengths
