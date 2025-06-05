99f5930a679e1cafd4d4f1d6ebeee9448d2e394b
Ha-YeJin <ziny7505k@gmail.com>
2025-06-05 13:06:12 +0900

    feat(preprocessing): add frame_utils module and modularize feature/label padding

---
+++ preprocessing/frame_utils.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+def pad_or_truncate_feature(feature, max_frames, fill_value=0):
+    if len(feature) > max_frames:
+        return feature[:max_frames]
+    elif len(feature) < max_frames:
+        pad_shape = (max_frames - len(feature), *feature.shape[1:]) if feature.ndim > 1 else (max_frames - len(feature),)
+        padding = np.full(pad_shape, fill_value)
+        return np.concatenate([feature, padding], axis=0)
+    return feature
+
+def seconds_to_frames(seconds, sr=16000, hop_length=160):
+    return int(seconds * sr / hop_length)
