c400c7a6dd53be5488a644e2b3dff8bd0c5d89a5
Ha-YeJin <ziny7505k@gmail.com>
2025-06-12 19:36:52 +0900

    refactor: remove unused get_max_lengths import

--- preprocessing/__init__.py
+++ preprocessing/__init__.py
@@ -1,5 +1,5 @@
 from .split_dataset import split_metadata
 from .feature_extraction import extract_features
-from .build_dataset import build_metadata_list, get_max_lengths
+from .build_dataset import build_metadata_list
 from .label_utils import create_phoneme_label, create_error_label
 from .frame_utils import pad_or_truncate_feature, seconds_to_frames
