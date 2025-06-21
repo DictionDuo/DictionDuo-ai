21c9e484a6098c0c5546dc161190d0693a01d5cb
Ha-YeJin <ziny7505k@gmail.com>
2025-06-21 12:54:37 +0900

    refactor(utils): organize utility imports for phoneme processing and configuration

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,3 +1,3 @@
-from .phoneme_utils import Korean, phoneme2index
+from .phoneme_utils import Korean, phoneme2index, convert_prompt_to_phoneme_sequence
 from .logger import setup_logger
 from .config_loader import convert_config_to_namespace
